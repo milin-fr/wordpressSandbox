d81a97aa3bcaf7eb7237a9f0ef5c70e029a4b7bc
unknown <m.ilin.fr@gmail.com>
2020-05-01 23:58:54 +0200

    linking js

--- wp-content/themes/mysandbox/functions.php
+++ wp-content/themes/mysandbox/functions.php
@@ -20,9 +20,20 @@ if ( ! function_exists( 'load_stylesheets' ) ) :
 		wp_register_style("bootstrap_css", get_template_directory_uri() . "/css/bootstrap.min.css", array(), false, "all");
 		wp_enqueue_style("bootstrap_css");
 
-		wp_register_style("style", get_template_directory_uri() . "/css/style.css", array(), false, "all");
+		wp_register_style("style", get_template_directory_uri() . "/style.css", array(), false, "all");
 		wp_enqueue_style("style");
 	}
 	
 endif;
 add_action("wp_enqueue_scripts", "load_stylesheets");
+
+if ( ! function_exists( 'load_js' ) ) :
+
+	function load_js() 
+	{
+		wp_register_script("app_js", get_template_directory_uri() . "/js/app.js", "", false, true);
+		wp_enqueue_script("app_js");
+	}
+	
+endif;
+add_action("wp_enqueue_scripts", "load_js");
