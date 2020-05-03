6deea338b1f75fd6c88479911fd549bf917f4efd
unknown <m.ilin.fr@gmail.com>
2020-05-03 20:51:30 +0200

    customising footer and giving credit to author

--- wp-content/themes/hitchcock-child/footer.php
+++ wp-content/themes/hitchcock-child/footer.php
@@ -2,8 +2,9 @@
 
 <div class="credits section-inner">
 			
-	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
-	<p class="theme-by"><?php _e( 'Theme by', 'hitchcock' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
+	<!--<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>-->
+	<p></p>
+	<p class="theme-by"><?php _e( 'Basé sur un thème créé par', 'hitchcock' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
 	
 	<div class="clear"></div>
 	
