e1a0f09482f882c0dcbe4cdc64ba96ea8ab871e4
unknown <m.ilin.fr@gmail.com>
2020-05-04 23:14:32 +0200

    myga working plugin

--- wp-content/plugins/myga/myga.php
+++ wp-content/plugins/myga/myga.php
@@ -11,6 +11,37 @@ function add_ga_menu_option()
 
 add_action('admin_menu','add_ga_menu_option');
 
+function ga_scripts_page()
+{
+    if(array_key_exists('submit_scripts_update', $_POST))
+    {
+        update_option('header_scripts', $_POST['header_scripts']);
 
+        ?>
+        <div id="setting-error-settings-updated" class="updated settings-error notice is-dismissible"><strong>Settings have been saved.</strong></div>
+        <?php
+    }
+    
+    $header_scripts = get_option('header_scripts',"none");
 
+    ?>
+    <div class="wrap">
+        <h2>Update Scripts</h2>
+        <form action="" method="post">
+            <label for="header_scripts">Header Scripts</label>
+            <textarea name="header_scripts" class="large-text"><?php echo $header_scripts;?></textarea>
+            <input type="submit" name="submit_scripts_update" class="button button-primary" value="Update scripts">
+        </form>
+    </div>
+    <?php
+}
+
+function display_header_scripts()
+{
+    $header_scripts = get_option('header_scripts','none');
+
+    echo $header_scripts;
+}
+
+add_action('wp_head', 'display_header_scripts')
 ?>
